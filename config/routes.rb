4a81a61ba06986a39c8453933deafd5226b9cad6
WinCodeX <glenwinterg970@gmail.com>
2023-03-03 18:05:55 +0300

    Devise and Routes for nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
